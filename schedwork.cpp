0e6785605f4a4a2b7a432a19f5f4235579f86faa
Peyton Crawford <pcrawfor@usc.edu>
2025-04-25 11:56:51 -0700

    Finally done and passed all the tests for both

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,14 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t& m,
+    DailySchedule& sched,
+    std::vector<Worker_T>& numShiftsList, // List of days scheduled for each worker based on ID
+    Worker_T largestIDScheduled, // Last ID we scheduled because we go in order
+    size_t currentDay // Current day we are working through because we go in order
+);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +44,52 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
-
-
-
-
+    for(size_t i = 0; i < avail.size(); i++){
+        sched.push_back(std::vector<Worker_T>(dailyNeed, INVALID_ID)); // Build "empty" matrix of size n x d
+    }
+    std::vector<Worker_T> numShiftsList(avail[0].size(), 0); // List of number of days that each worker has worked
+    return scheduleHelper(avail, maxShifts, sched, numShiftsList, INVALID_ID, 0);
 }
 
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t& m,
+    DailySchedule& sched,
+    std::vector<Worker_T>& numShiftsList,
+    Worker_T largestIDScheduled,
+    size_t currentDay
+)
+{
+    // Has anyone been scheduled more than m times?
+    if(std::find(numShiftsList.begin(), numShiftsList.end(), m + 1) != numShiftsList.end())
+        return false;
+
+    Worker_T k = numShiftsList.size(); // Number of workers
+
+    for(size_t i = currentDay; i < avail.size(); i++){
+        // Is this day filled up yet?
+        std::vector<Worker_T>::iterator it = std::find(sched[i].begin(), sched[i].end(), INVALID_ID);
+        if(it != sched[i].end()){
+            // Only go through IDs we haven't used for this day
+            for(Worker_T j = largestIDScheduled + 1; j < k; j++){
+                if(avail[i][j] == 1){ // Worker is available this day
+                    *it = j;
+                    numShiftsList[j]++;
+                    if(!scheduleHelper(avail, m, sched, numShiftsList, j, i)){
+                        numShiftsList[j]--; //Revert list then continue trying IDs
+                        continue;
+                    } else{
+                        return true; // It did work
+                    }
+                }
+            }
+            *it = INVALID_ID;
+        } 
+        // If there's still an invalid ID return false
+        if(std::find(sched[i].begin(), sched[i].end(), INVALID_ID) != sched[i].end()){
+            return false;
+        }
+        largestIDScheduled = INVALID_ID; //Moving to the next day
+    }
+    return true;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,15 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordleHelper(
+    std::string& in, 
+    std::string& floating, 
+    const std::set<std::string>& dict, 
+    std::set<std::string>& retSet, //Set of words we're returning
+    size_t& numDashes, 
+    size_t idx,   // Index of letter that we're working through
+    size_t& nonFloatCount
+);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +30,73 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    std::set<std::string> retSet;
+    std::string inputCopy = in;
+    std::string floatingCopy = floating;
+    size_t numDashes = 0;
+    size_t nonFloatCount = 0;
+    // Count number of dashes
+    for(size_t i = 0; i < in.size(); i++){
+        if(in[i] == '-')
+            numDashes++;
+    }
+
+    wordleHelper(inputCopy, floatingCopy, dict, retSet, numDashes, 0, nonFloatCount);
+    return retSet;
 
 }
 
 // Define any helper functions here
+void wordleHelper(
+    std::string& in,
+    std::string& floating,
+    const std::set<std::string>& dict,
+    std::set<std::string>& retSet,
+    size_t& numDashes,
+    size_t idx,
+    size_t& nonFloatCount
+)
+{
+    if(numDashes == 0){ //The word is full
+        if(dict.find(in) != dict.end()) //It is actually a word
+            retSet.insert(in); // Insert into set
+    } else{
+        //Locate first dash
+        while(idx < in.size() - 1 && in[idx] != '-'){
+            idx++;
+        }
+        //Try all floating numbers here
+        for(size_t j = 0; j < floating.size(); j++){
+            if(floating[j] != ' '){
+                in[idx] = floating[j];
+                numDashes--;
+                floating[j] = ' ';
+                wordleHelper(in, floating, dict, retSet, numDashes, ++idx, nonFloatCount);
+                
+                //Revert changes
+                floating[j] = in[--idx];
+                in[idx] = '-';
+                numDashes++;
+            }
+        }
+        //Count how many floats are left
+        size_t floatRemaining = 0;
+        for(size_t y = 0; y < floating.size(); y++){
+            if(floating[y] != ' ')
+                floatRemaining++;
+        }
+        //Only start adding other letters if there's available space
+        if(numDashes > floatRemaining){
+            for(size_t k = 0; k < 26; k++){
+                in[idx] = 'a' + k;
+                numDashes--;
+                wordleHelper(in, floating, dict, retSet, numDashes, ++idx, ++nonFloatCount);
+                
+                //Revert changes
+                nonFloatCount--;
+                in[--idx] = '-';
+                numDashes++;
+            }
+        }
+    }
+}
